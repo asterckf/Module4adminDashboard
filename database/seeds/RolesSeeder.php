1ae19a9ed8b5474da4bfb8939639fed3baab0120
asterckf <asterckf@gmail.com>
2017-01-10 02:47:48 +0800

    role seeded

---
+++ database/seeds/RolesSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class RolesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+//        DB::table('roles')->delete();
+        DB::table('roles')->insert(array(
+            array('name'=>'Accountant','description'=>'Analyze and compile record of asset,
+            liability and capital.Summarize balance, profit and loss statement'),
+            array('name'=>'Accountant Delegate','description'=>'Working with spreadsheet,sales and purchase ledgers and journals.
+            Sorting out incoming and outgoing daily transaction. Calculate and check to make sure payment and biling is record
+            correctly'),
+            array('name'=>'Auditor','description'=>'Planning for annual audit.Finalize audit record.
+            Examine company accounts and financial control system'),
+            array('name'=>'Auditor Delegate','description'=>'Review accountant documentation. Ensure validity and legality
+            of financial record from accountant'),
+            array('name'=>'Company Director','description'=>'Monitor job assigments. Analyze, review and develop
+            business strategies from audit report.'),
+            array('name'=>'Company Secretary','description'=>'Prepare,approve,sign and seal agreements leases, legal form
+            and official documents. Implement the policies formulated by the directors'),
+
+        ));
+    }
+}
